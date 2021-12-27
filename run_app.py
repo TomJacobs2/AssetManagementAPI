65cd0658fce8097c8bcbbecc5fd401720c75444f
TomJacobs2 <tjacobs@spidercreekstudios.com>
2021-12-26 21:51:31 -0600

    Initial Project Creatation
    Create the initial project

---
+++ run_app.py
@@ -0,0 +1,9 @@
+#  Copyright (c) Thomas Jacobs. All Rights Reserved.
+
+
+def main():
+    print("hello run app")
+
+
+if __name__ == "__main__":
+    main()
---
+++ run_tests.py
@@ -0,0 +1,8 @@
+#  Copyright (c) Thomas Jacobs. All Rights Reserved.
+
+def main():
+    print("Hello Test Run")
+
+
+if __name__ == "__main__":
+    main()
